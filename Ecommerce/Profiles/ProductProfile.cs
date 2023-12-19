5058cc601d5a167380446883c685c6f8fe47b0e8
Gordon <gordongichini1@gmail.com>
2023-12-19 05:10:53 +0300

    Product Profile created

---
+++ Ecommerce/Profiles/ProductProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Ecommerce.Models;
+using Ecommerce.Models.Dtos;
+
+namespace Ecommerce.Profiles
+{
+    public class ProductProfile:Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<AddProductDto, Product>().ReverseMap();
+            CreateMap<AddOrderDto, Order>();
+            CreateMap<AddUserDto, User>();
+        }
+
+    }
+}
